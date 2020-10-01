bca1a08339cddfca22a92d4f71e87c44d36a1114
blakecrowther <blakecrowther7@gmail.com>
2020-09-30 18:05:51 -0700

    Finish react saga in burger builder

--- React/burger-builder/src/store/actions/index.js
+++ React/burger-builder/src/store/actions/index.js
@@ -2,9 +2,21 @@ export {
   addIngredient,
   removeIngredient,
   initIngredients,
+  setIngredients,
+  fetchIngredientsFailed,
 } from "./burgerBuilder";
 
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  fetchOrdersStart,
+  fetchOrdersFail,
+  fetchOrdersSuccess,
+  purchaseBurgerStart,
+  purchaseBurgerSuccess,
+  purchaseBurgerFail,
+} from "./order";
 
 export {
   auth,
@@ -12,4 +24,8 @@ export {
   setAuthRedirectPath,
   authCheckState,
   logoutSucceed,
+  authStart,
+  authSuccess,
+  authFail,
+  checkAuthTimeout,
 } from "./auth";
--- React/burger-builder/src/store/sagas/index.js
+++ React/burger-builder/src/store/sagas/index.js
@@ -1,8 +1,30 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, all, takeLatest } from "redux-saga/effects";
 
 import * as actionTypes from "../actions/actionTypes";
-import { logoutSaga } from "./auth";
+import {
+  logoutSaga,
+  checkAuthTimeoutSaga,
+  authUserSaga,
+  authCheckStateSaga,
+} from "./auth";
+
+import { initIngredientSaga } from "./burgerBuilder";
+import { fetchOrdersSaga, purchaseBurgerSaga } from "./order";
 
 export function* watchAuth() {
-  yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+  yield all([
+    takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+    takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+    takeEvery(actionTypes.AUTH_USER, authUserSaga),
+    takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga),
+  ]);
+}
+
+export function* watchBurgerBuilder() {
+  yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientSaga);
+}
+
+export function* watchOrder() {
+  yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
